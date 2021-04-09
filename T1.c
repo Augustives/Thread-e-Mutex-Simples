13ddd9a4c7b44f572dc2ed206dd2668c51763ecd
Augusto <augusto1408@hotmail.com>
2021-04-09 14:15:00 -0300

    Update T1.c
    Algumas mudancas

--- T1.c
+++ T1.c
@@ -7,8 +7,8 @@
 
 
 #define tamanho_array 50
-#define n_threads_clientes 100
-#define n_threads_voluntarios 100
+#define n_threads_clientes 25
+#define n_threads_voluntarios 25
 
 pthread_mutex_t mutex_disponiveis;
 pthread_mutex_t mutex_reparos;
@@ -41,8 +41,8 @@ void cliente_doa_roupa(char roupa[]) {
 	for (i=0; i<tamanho_array; i++) {
 		if (roupas_reparo[i].codigo == -1) {
 			roupas_reparo[i] = str_roupa;
-			printf("Cliente doou roupa usada: %s\nID:%d\n", roupa, str_roupa.codigo);
-			break;
+			printf("Cliente doou roupa usada: %s - ID:%d\n", roupa, str_roupa.codigo);
+			return;
 		}
 	}
 }
@@ -56,26 +56,33 @@ void cliente_compra_roupa(char roupa[]) {
 		int value;
         value = strcmp(roupa , roupas_disponiveis[i].modelo);  
 		if (value == 0) {
-			printf("Cliente comprou roupa: %s\nID:%d\n", roupa, roupas_disponiveis[i].codigo);
+			printf("Cliente comprou roupa: %s - ID:%d\n", roupa, roupas_disponiveis[i].codigo);
 			frequencia[i] = 0;
 			struct Roupa str_roupa;
             str_roupa.codigo = -1;
 			roupas_disponiveis[i] = str_roupa;
-			break;
+			return;
 		}
-	}	
+	}
+	printf("Cliente roupa nao encontrada em disponiveis: %s\n", roupa);	
 }
 
 void voluntario_move_roupa() {
     int i;
     for (i=0; i<tamanho_array; i++) {
         if (roupas_reparo[i].codigo != -1) {
-	        roupas_disponiveis[i] = roupas_reparo[i];
-			struct Roupa str_roupa;
-            str_roupa.codigo = -1;
-			roupas_reparo[i] = str_roupa;
-			printf("Voluntario moveu para disponivel: %s\nID:%d\n", roupas_disponiveis[i].modelo, roupas_disponiveis[i].codigo);
-			break;
+        	struct Roupa str_roupa;
+        	str_roupa.codigo = roupas_reparo[i].codigo;
+        	strcpy(str_roupa.modelo, roupas_reparo[i].modelo);
+        	str_roupa.preco = roupas_reparo[i].preco;
+        	strcpy(str_roupa.tamanho, roupas_reparo[i].tamanho);
+	        roupas_disponiveis[i] = str_roupa;
+
+			struct Roupa str_roupa2;
+            str_roupa2.codigo = -1;
+			roupas_reparo[i] = str_roupa2;
+			printf("Voluntario moveu para disponivel: %s - ID:%d\n", roupas_disponiveis[i].modelo, roupas_disponiveis[i].codigo);
+			return;
 		}
     }
 }
@@ -93,8 +100,8 @@ void voluntario_doa_nova(char roupa[]) {
 	for (i=0; i<tamanho_array; i++) {
 		if (roupas_disponiveis[i].codigo == -1) {
 			roupas_disponiveis[i] = str_roupa;
-			printf("Voluntario doou roupa nova: %s\nID:%d\n", roupa, str_roupa.codigo);
-			break;
+			printf("Voluntario doou roupa nova: %s - ID:%d\n", roupa, str_roupa.codigo);
+			return;
 		}
 	}    
 }
@@ -111,7 +118,7 @@ void voluntario_remove_velha() {
         }
     }
     if (roupas_disponiveis[id].codigo != -1) {
-        printf("Voluntario removeu roupa indesejada: %s\nID:%d\n", roupas_disponiveis[id].modelo, roupas_disponiveis[id].codigo);
+        printf("Voluntario removeu roupa indesejada: %s - ID:%d\n", roupas_disponiveis[id].modelo, roupas_disponiveis[id].codigo);
         struct Roupa str_roupa;
         str_roupa.codigo = -1;
 	    roupas_disponiveis[id] = str_roupa;
@@ -154,12 +161,29 @@ void *t_function_voluntario(void *arg) {
         pthread_mutex_unlock(&mutex_reparos);
     } else if (i == 1) {
         int i = rand() % 10;
+        pthread_mutex_lock(&mutex_disponiveis);
         voluntario_doa_nova(roupas[i]);
+        pthread_mutex_unlock(&mutex_disponiveis);
     } else {
         pthread_mutex_lock(&mutex_disponiveis);
         voluntario_remove_velha();
         pthread_mutex_unlock(&mutex_disponiveis);
     }
+}
+
+void test_case() {
+    cliente_doa_roupa(roupas[0]);
+    cliente_doa_roupa(roupas[1]);
+    voluntario_doa_nova(roupas[2]);
+    
+    cliente_compra_roupa(roupas[0]);
+    cliente_compra_roupa(roupas[2]);
+    
+    voluntario_move_roupa();
+    cliente_compra_roupa(roupas[0]);
+
+    voluntario_move_roupa();
+    voluntario_remove_velha();
 }
 
 
@@ -172,6 +196,7 @@ int main(int argc, char const *argv[]) {
         str_roupa.codigo = -1;
         roupas_disponiveis[i] = str_roupa;
         roupas_reparo[i] = str_roupa;
+        frequencia[i] = 0;
     }
     
     pthread_mutex_init(&mutex_disponiveis, NULL);
